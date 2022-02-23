ea80048ceee6d304d5175f3a495b445427f5b162
Husky20 <husky20205@wp.pl>
2022-02-23 10:05:56 +0100

    Repository initialization
    Creating a C# project and writing "Hello world".

---
+++ Game/Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Game {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Hello World!");
+            Thread.Sleep(2000);
+        }
+    }
+}
